a0cc57018662ee58f7c3c1cb14bc7e70cda8bfed
Geovana <geo.maria117@gmail.com>
2025-08-27 16:41:28 -0300

    ajuste controller e service

--- src/main/java/com/resumo/texto/application/service/GenerateSummaryService.java
+++ src/main/java/com/resumo/texto/application/service/GenerateSummaryService.java
@@ -1,37 +1,39 @@
 package com.resumo.texto.application.service;
 
 import com.resumo.texto.adapter.output.ISummaryFeignClient;
+import com.resumo.texto.application.mapper.SummaryMapper;
 import com.resumo.texto.application.port.input.IGenerateSummary;
-import com.resumo.texto.application.usecase.SummaryTextUseCase;
 import com.resumo.texto.domain.exception.SummaryException;
 import com.resumo.texto.dto.request.SummaryRequestDto;
-import com.resumo.texto.dto.request.TextUserRequestDto;
+import com.resumo.texto.dto.response.SummaryItemResponse;
 import com.resumo.texto.dto.response.SummaryResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class GenerateSummaryService implements IGenerateSummary {
 
     @Autowired
-    SummaryTextUseCase useCase;
-
-    @Autowired
     ISummaryFeignClient feignClient;
 
+    @Autowired
+    SummaryMapper mapper;
+
     @Override
-    public ResponseEntity<SummaryResponseDto> generateSummary(TextUserRequestDto textUser, String token) {
+    public ResponseEntity<SummaryResponseDto> generateSummary(SummaryRequestDto textUser, String token) {
         try{
             log.info("Iniciando chamada à IA");
             log.info("Sumarização o texto: {}", textUser);
-            SummaryRequestDto prompt = useCase.formatText(textUser.getInputText());
-            SummaryResponseDto response = feignClient.generateSummary(prompt, token);
+            List<SummaryItemResponse> summaryItens = feignClient.generateSummary(textUser, token);
+            SummaryResponseDto response = mapper.toResponseDto(summaryItens);
             log.info("Finalizando sumarização");
             return ResponseEntity.ok(response);
-        }catch (SummaryException e){
+        }catch (Exception e){
             log.error("ocorreu um erro durante a execução da chamada da IA");
             throw new SummaryException(e.getMessage(), e.getCause());
         }
