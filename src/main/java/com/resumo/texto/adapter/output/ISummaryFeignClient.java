28aca5d8faed726aaea29f8f28ec37d55c7b5951
Geovana <geo.maria117@gmail.com>
2025-08-27 16:41:47 -0300

    ajuste das interfaces

--- src/main/java/com/resumo/texto/adapter/output/ISummaryFeignClient.java
+++ src/main/java/com/resumo/texto/adapter/output/ISummaryFeignClient.java
@@ -1,16 +1,18 @@
 package com.resumo.texto.adapter.output;
 
 import com.resumo.texto.dto.request.SummaryRequestDto;
-import com.resumo.texto.dto.response.SummaryResponseDto;
+import com.resumo.texto.dto.response.SummaryItemResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import java.util.List;
+
 @FeignClient (name = "${feign.api.name}", url = "${feign.api.url}")
 public interface ISummaryFeignClient {
 
     @PostMapping
-    SummaryResponseDto generateSummary(@RequestBody SummaryRequestDto text,
-                                       @RequestHeader("Authorization") String authorization);
+    List<SummaryItemResponse> generateSummary(@RequestBody SummaryRequestDto text,
+                                              @RequestHeader("Authorization") String authorization);
 }
