6482ba867110ed4145dbdfaaa547f4d09f2b590b
Geovana <geo.maria117@gmail.com>
2025-08-27 16:42:20 -0300

    criação de um novo response para ser mapeado

---
+++ src/main/java/com/resumo/texto/dto/response/SummaryItemResponse.java
@@ -0,0 +1,13 @@
+package com.resumo.texto.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SummaryItemResponse {
+
+    private String summary_text;
+}
